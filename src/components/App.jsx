6113da5e48cfcdbf0ac83c0b659f4733bbe948b1
SmithCalimero <a21280192@isec.pt>
2023-04-25 16:49:02 +0100

    Styling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { CssBaseline } from '@mui/material';
 import { Routes, Route } from 'react-router-dom';
 
+import useStyles from './styles';
+
 /* THESE LINES CAN BE SIMPLIFIED WITH index.js AND WITH THE LINE BELOW THEM
 import Navbar from './Navbar/Navbar';
 import Profile from './Profile/Profile';
@@ -12,19 +14,24 @@ import Actor from './Actor/Actor';
 
 import { Navbar, Profile, Movie, MovieInformation, Actor } from './index';
 
-const App = () => (
-  <div>
-    <CssBaseline />
-    <main>
+const App = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Movie />} />
-        <Route path="profile/:id" element={<Profile />} />
-        <Route path="movie/:id" element={<MovieInformation />} />
-        <Route path="actor/:id" element={<Actor />} />
-      </Routes>
-    </main>
-  </div>
-);
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Routes>
+          <Route path="/" element={<Movie />} />
+          <Route path="profile/:id" element={<Profile />} />
+          <Route path="movie/:id" element={<MovieInformation />} />
+          <Route path="actor/:id" element={<Actor />} />
+        </Routes>
+      </main>
+    </div>
+  );
+};
 
 export default App;
